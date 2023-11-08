3f8f974ff91278dd6afc1aaf28ec5dd7966185ba
engrmhk <engrmhk@yahoo.com>
2023-11-08 11:20:12 +0400

    plugin upload

---
+++ samm.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Plugin Name:       SAMM
+ * Plugin URI:        https://www.mudassir.co/my-work/
+ * Description:       Gutenberg blocks
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mudassir Hassan Khan
+ * Author URI:        https://mudassir.co/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       samm
+ * Domain Path:       /languages
+ */
+
